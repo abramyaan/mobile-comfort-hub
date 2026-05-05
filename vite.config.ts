398438f287c635cd3c3d248648e39639c8e6d6d4
ero <abramyan_ero@mail.ru>
2026-05-05 16:17:00 +0500

    feat: fix tailwind compilation and assets paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite"; // 1. Добавь импорт
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
       routesDirectory: "./src/routes",
       generatedRouteTree: "./src/routeTree.gen.ts",
-      // Заменяем проблемный experimental флаг на новый стабильный
-      autoCodeSplitting: true, 
+      autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(), // 2. Добавь плагин СЮДА
     tsconfigPaths(),
   ],
   base: "/mobile-comfort-hub/",
